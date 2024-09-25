5636ea82004cd4d915a087e8398aadb33be4acc1
martin zhang <zk12@live.cn>
2024-09-25 22:04:53 +0800

    isOdd canBeDividedBy357

--- Proj0302/Proj030201/01.cpp
+++ Proj0302/Proj030201/01.cpp
@@ -47,3 +47,52 @@ using namespace std;
 
 	 printf("The Abs float number of input is %0.2f", output);
  }
+
+ void isOdd() {
+	
+	 int iNum;
+	 string strResult;
+
+	 printf("Please input a number: ");
+	 scanf_s("%d", &iNum);
+
+	 if (iNum % 2 == 0) {
+		 strResult = "even";
+	 }
+	 else {
+		 strResult = "odd";
+	 }
+
+	 printf("The number of %d is %s", iNum, strResult.c_str());
+
+ }
+
+ void canBeDividedBy357() {
+
+	 int iNum;
+	 string strResult="The number of %d can be divided by ";
+	 bool isDivided = false;
+
+
+	 printf("Please input a number: ");
+	 scanf_s("%d", &iNum);
+
+	 if (iNum % 3 == 0) {
+		 strResult.append("3 ");
+		 isDivided = true;
+	 }
+	 if (iNum % 5 == 0) {
+		 strResult.append("5 ");
+		 isDivided = true;
+	 }
+	 if (iNum % 7 == 0) {
+		 strResult.append("7 ");
+		 isDivided = true;
+	 }
+	 if (!isDivided) {
+		 strResult = "The number of %d cannot be divided by 3 5 7";
+	 }
+
+	 printf(strResult.c_str(), iNum);
+
+ }
--- Proj0302/Proj030201/0302.cpp
+++ Proj0302/Proj030201/0302.cpp
@@ -1,22 +1,52 @@
 #include<iostream>
 using namespace std;
 
+void process();
+
 void judgeNumber();
 void getAbs();
+void isOdd();
+void canBeDividedBy357();
 
 int main() {
+
+	int iNum;
+
+	process();
+
+	while (true) {
+		
+		printf("\nPlease input 1 to continue, otherwise program will break: ");
+		scanf_s("%d", &iNum);
+		if (iNum == 1) {
+			
+			printf("\n");
+			process();
+		}
+		else {
+			
+			break;
+		}
+	}
+	
+
+}
+
+void process() {
 	
 	int iNum;
 
 	printf("1- JudgeNumber \n");
 	printf("2- GetAbs \n");
+	printf("3- IsOdd \n");
+	printf("4- canBeDividedBy357 \n");
 
-	printf("Please select a number: ");
+	printf("\nPlease select a number: ");
 	scanf_s("%d", &iNum);
 
 	switch (iNum)
 	{
-	case 1: 
+	case 1:
 		judgeNumber();
 		break;
 
@@ -24,8 +54,15 @@ int main() {
 		getAbs();
 		break;
 
+	case 3:
+		isOdd();
+		break;
+
+	case 4:
+		canBeDividedBy357();
+		break;
+
 	default:
 		break;
 	}
-	
 }
